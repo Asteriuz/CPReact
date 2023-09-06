f66c450c02e9434f708aa97b4f7047453a3b1e46
Asteriuz <gutocebola@gmail.com>
2023-09-06 18:39:10 -0300

    Added home

--- src/routes/Home/index.jsx
+++ src/routes/Home/index.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './home.css';
 
 export default function index() {
   return (
-    <div>Home</div>
-  )
+    <main className='home'>
+      <section id="sale1">
+        <div class='sale-text'>
+          <h1>PROMOÇÃO SURPRESA DE INVERNO</h1>
+          <h2>TEMPO LIMITADO (Aplicado em toda loja)</h2>
+        </div>
+        <div class='sale-num'>
+          <h1>20%</h1>
+          <h1>OFF</h1>
+          </div>
+      </section>
+      <Link className='btn' to="/product">Veja todos nossos aparelhos</Link>
+    </main>
+  );
 }
